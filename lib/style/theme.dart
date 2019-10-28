a8d419827431ff5526287288a5dcd95cfa39df16
leielyq <leielyq123@gmail.com>
2019-10-28 15:48:53 +0800

    调整属性的位置

--- lib/style/theme.dart
+++ lib/style/theme.dart
@@ -13,14 +13,7 @@ class Colors {
   static const Color bgColor = const Color(0xFFf2f2f2);
   static const Color textHint = const Color(0xFFcccccc);
   static const Color textPoint = const Color(0xFF808080);
-
-  static const TextStyle hintStyle = const TextStyle(color: textHint,fontSize: 14);
-  static const TextStyle pointStyle = const TextStyle(color: textPoint,fontSize: 16);
-  static const TextStyle chooseStyle = const TextStyle(color: textPoint,fontSize: 16);
-  static const TextStyle nameStyle = const TextStyle(color: textPoint,fontSize: 10);
-  static const TextStyle titleStyle = const TextStyle(color: black,fontSize: 16);
-  static const TextStyle contextStyle = const TextStyle(color: black,fontSize: 14);
-
+  static const Color bg = const Color(0xFFF1F4Fb);
 
   static const primaryGradient = const LinearGradient(
     colors: const [loginGradientStart, loginGradientEnd],
@@ -30,9 +23,17 @@ class Colors {
   );
 }
 
+class Style{
+  static const TextStyle hintStyle = const TextStyle(color: textHint,fontSize: 14);
+  static const TextStyle pointStyle = const TextStyle(color: textPoint,fontSize: 16);
+  static const TextStyle chooseStyle = const TextStyle(color: textPoint,fontSize: 16);
+  static const TextStyle nameStyle = const TextStyle(color: textPoint,fontSize: 10);
+  static const TextStyle titleStyle = const TextStyle(color: black,fontSize: 16);
+  static const TextStyle contextStyle = const TextStyle(color: black,fontSize: 14);
+}
+
+
 class Device{
   const Device();
-
-
 
 }
